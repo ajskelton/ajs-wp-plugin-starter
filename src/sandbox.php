3821b7670cb2223903142141dd16945f3308fad5
Anthony Skelton <anthony@redcartstudios.com>
2016-12-05 15:50:06 -0800

    Inital Commit

--- src/sandbox.php
+++ src/sandbox.php
@@ -7,17 +7,152 @@
 namespace AJSkelton;
 
 
-add_action( 'loop_start', __NAMESPACE__ . '\demo' );
-
 /*
-* Demo - Testing Purposes.
+* 'plugins_loaded'
+* 'after_setup_theme'
+* 'init'
 *
-* @since 1.0.0
+* 'loop_start'
+* 'loop_end'
 *
-* @return void
  */
-function demo() {
-	var_dump( get_the_ID() );
-	echo get_the_ID();
-	d( get_the_ID() );
+
+// add_action( 'loop_start', function() {
+// 	d( 'loop start is firing' );
+// 	add_action( 'init', __NAMESPACE__ . '\testing_init_action', 15 );
+// });
+
+
+// function testing_init_action() {
+// 	global $wp_filter;
+
+// 	d( $wp_filter['init']);
+// 	ddd();
+// }
+
+
+// class Init {
+
+// 	public function __construct() {
+
+// 		// do some stuff
+		
+// 		$this->init_events();
+// 	}
+
+// 	protected function init_events() {
+// 		add_action( 'init', 'AJSkelton\Init::testing_loop_start_action', 15 );
+// 	}
+
+// 	static function testing_loop_start_action() {
+// 		global $wp_filter;
+
+// 		echo 'static method';
+
+// 		d( $wp_filter['init']);
+// 	}
+// }
+
+// add_action( 'init', function() {
+// 	echo 'closure<br>';
+// }, 20);
+
+// $init = new Init();
+// 
+
+// add_filter( 'init', __NAMESPACE__ . '\testing_loop_start_action', 15 );
+// function testing_loop_start_action() {
+// 	global $wp_filter;
+
+// 	d( $wp_filter['init'] );
+// }
+// 
+
+
+/* WordPress Plugin API Introduction Registering Events */
+
+// add_filter( 'the_title', __NAMESPACE__ . '\add_prefix_to_the_post_title', 100, 2);
+
+// function add_prefix_to_the_post_title( $post_title ) {
+
+// 	global $wp_filter;
+
+// 	d( func_get_arg( 1 ) );
+// 	d( func_get_args() );
+
+// 	// d( $post_title );
+// 	// ddd( $wp_filter['the_title'] );
+
+// 	return 'Anthony ';
+// }
+
+
+// add_filter( 'the_title', __NAMESPACE__ . '\demo_priority', 10, 2 );
+
+// function demo_priority( $post_title, $post_id ) {
+
+// 	global $wp_filter;
+
+// 	add_prefix_to_the_post_title( $post_title, $post_id );
+
+// 	return 'Called me first!';
+// }
+
+
+
+/* WordPress Getting Post Types */
+// add_action( 'loop_start', __NAMESPACE__ . '\fetch_the_post_types' );
+// function fetch_the_post_types() {
+// 	global $wp_post_types;
+// //	d( $wp_post_types );
+// 	$args = array(
+// 		'_builtin' => false,
+// 		'public' => false,
+// 	);
+// 	$post_types = get_post_types( $args, 'names', 'or' );
+// 	ddd( $post_types );
+// }
+
+/* Adding / Editing post type features */
+
+add_action( 'init' , __NAMESPACE__ . '\testing_post_type_features', 9999 );
+
+function testing_post_type_features() {
+	// d( post_type_supports( 'page', 'excerpt' ) );
+	// d( post_type_supports( 'post', 'page-attributes' ) );
+
+	add_post_type_support( 'page', 'excerpt' );
+	add_post_type_support( 'post', 'page-attributes' );
+	// ddd( post_type_supports( 'page', 'attributes' ) );
+	// 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
